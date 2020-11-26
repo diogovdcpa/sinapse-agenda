f0a88bcad070d587c2967b58dcba8820df2626ff
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 11:23:46 -0300

    url

--- config/config.php
+++ config/config.php
@@ -11,4 +11,4 @@ define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
 define('URL_BASE', 'http://'.$_SERVER["HTTP_HOST"]);
-define('ASSETS', 'http://'.$_SERVER["HTTP_HOST"].'assets/');
+define('ASSETS', 'http://'.$_SERVER["HTTP_HOST"].'/assets/');
