e6e1cd755f48bcb64720a75d131685781288ce72
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 11:26:31 -0300

    mod url

--- config/config.php
+++ config/config.php
@@ -10,5 +10,5 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://'.$_SERVER["HTTP_HOST"]);
-define('ASSETS', 'http://'.$_SERVER["HTTP_HOST"].'/assets/');
+define('URL_BASE', 'http://agenda-sinapse.herokuapp.com/');
+define('ASSETS', 'http://agenda-sinapse.herokuapp.com/assets/');
